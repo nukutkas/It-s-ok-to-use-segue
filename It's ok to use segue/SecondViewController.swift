575fa3f168d0a2cbe048a7e00011f1abeb1447f6
Nikita Kochetov <nothsmple@mail.ru>
2021-04-12 23:25:46 +0300

    Perform segue preparation

---
+++ It's ok to use segue/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  It's ok to use segue
+//
+//  Created by Татьяна Кочетова on 12.04.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    var performData = ""
+    
+    @IBOutlet var dataLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
