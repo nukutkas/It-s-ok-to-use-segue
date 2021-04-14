b2689769614e2a1d82f1f6b97f9121fc788080df
Nikita Kochetov <nothsmple@mail.ru>
2021-04-14 22:54:53 +0300

    grandpaVC added

---
+++ It's ok to use segue/GrandpaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GrandpaViewController.swift
+//  It's ok to use segue
+//
+//  Created by Татьяна Кочетова on 14.04.2021.
+//
+
+import UIKit
+
+class GrandpaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
